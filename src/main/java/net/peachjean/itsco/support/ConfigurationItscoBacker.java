0a0861621e5ce684b01009a8bcc3bb6e09a7499e
Jared Bunting <jared.bunting@peachjean.com>
2013-03-23 13:13:25 -0500

    Removing BackedInstantiatorFactory - it was excessive.

---
+++ src/main/java/net/peachjean/itsco/support/ConfigurationItscoBacker.java
@@ -0,0 +1,69 @@
+package net.peachjean.itsco.support;
+
+import org.apache.commons.configuration.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ConfigurationItscoBacker<I> implements ItscoBacker<I> {
+
+    // contains values that handle reloading on their own - these are often more expensive to create so we
+    // don't want to recreate them on every get call
+    private final Map<String, Object> cachedValues;
+    private final Configuration context;
+    private final Iterable<FieldResolutionStrategy> strategies;
+    private I containing;
+
+    public ConfigurationItscoBacker(Configuration context, Iterable<FieldResolutionStrategy> strategies) {
+        this.context = context;
+        this.strategies = strategies;
+        cachedValues = new HashMap<String, Object>();
+    }
+
+    @Override
+    public void setContaining(I containing) {
+        this.containing = containing;
+    }
+
+    @Override
+    public <T> T lookup(final String name, final Class<T> lookupType) {
+        this.validateState();
+        FieldResolutionStrategy resolutionStrategy = this.determineStrategy(lookupType);
+        if (resolutionStrategy.isContextBacked()) {
+            if (cachedValues.containsKey(name)) {
+                return lookupType.cast(cachedValues.get(name));
+            }
+        }
+        final T resolved = resolutionStrategy.resolve(name, lookupType, context, containing);
+        if (resolved == null) {
+            throw new IllegalStateException("No value for " + name);
+        }
+        if (resolutionStrategy.isContextBacked()) {
+            cachedValues.put(name, resolved);
+        }
+        return resolved;
+    }
+
+    @Override
+    public <T> T lookup(final String name, final Class<T> lookupType, final T defaultValue) {
+        this.validateState();
+        FieldResolutionStrategy resolutionStrategy = this.determineStrategy(lookupType);
+        final T resolved = resolutionStrategy.resolve(name, lookupType, context, containing);
+        return resolved != null ? resolved : defaultValue;
+    }
+
+    private void validateState() {
+        if(this.containing == null) {
+            throw new IllegalStateException("A containing object must be set before the backer is usable.");
+        }
+    }
+
+    protected <T> FieldResolutionStrategy determineStrategy(final Class<T> lookupType) {
+        for (FieldResolutionStrategy strategy : strategies) {
+            if (strategy.supports(lookupType)) {
+                return strategy;
+            }
+        }
+        throw new IllegalStateException("No strategy to support type " + lookupType.getName());
+    }
+}
--- src/main/java/net/peachjean/itsco/support/DefaultItscoFactory.java
+++ src/main/java/net/peachjean/itsco/support/DefaultItscoFactory.java
@@ -2,114 +2,69 @@ package net.peachjean.itsco.support;
 
 import org.apache.commons.configuration.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
  */
 public class DefaultItscoFactory implements ItscoFactory {
 
-    private final BackedInstantiatorFactory backedInstantiatorFactory = new BackedInstantiatorFactory();
-    private final FieldResolutionStrategy[] strategies = {
+    private final ImplementationGenerator implementationGenerator = new AsmImplementationGenerator();
+
+    private final List<FieldResolutionStrategy> strategies = Arrays.asList(
             StringResolutionStrategy.INSTANCE,
             ValueOfResolutionStrategy.INSTANCE,
             new ItscoResolutionStrategy(this)
-    };
+    );
 
+    private final SimpleCache<Class<?>, Instantiator<?>> instantiatorCache = SimpleCache.build(new SimpleCache.Loader<Class<?>, Instantiator<?>>() {
+        @Override
+        public Instantiator<?> load(Class<?> key) {
+            return DefaultItscoFactory.this.createNewInstantiator(key);
+        }
+    });
 
     protected DefaultItscoFactory() {
     }
 
     @Override
     public <T> T create(Configuration config, Class<T> itscoClass) {
-        return backedInstantiatorFactory.lookupFunction(itscoClass).instantiate(createBacker(config, itscoClass));
+        return createGenerator(itscoClass).instantiate(config);
     }
 
     @Override
     public <T> T create(Configuration config, Class<T> itscoClass, InstantiationContext context) {
-        return backedInstantiatorFactory.lookupFunction(itscoClass).instantiate(createBacker(config, itscoClass), context);
+        return createGenerator(itscoClass).instantiate(config, context);
     }
 
     @Override
     public <T> Instantiator<T> createGenerator(final Class<T> itscoClass) {
+        return (Instantiator<T>) instantiatorCache.get(itscoClass);
+    }
+
+    private <T> Instantiator<T> createNewInstantiator(final Class<T> itscoClass) {
+        final Class<? extends T> implClass = implementationGenerator.implement(itscoClass);
+
+        final BackedInstantiator<T> instantiatior = new BackedInstantiatorImpl<T>(itscoClass, implClass);
+
         return new Instantiator<T>() {
+
             @Override
             public T instantiate(Configuration configuration) {
-                return create(configuration, itscoClass);
+
+                return instantiatior.instantiate(createBacker(configuration));
             }
 
             @Override
             public T instantiate(Configuration configuration, InstantiationContext context) {
-                return create(configuration, itscoClass, context);
+                return instantiatior.instantiate(createBacker(configuration), context);
+            }
+
+            private <T> ItscoBacker createBacker(final Configuration context) {
+                return new ConfigurationItscoBacker(context, strategies);
             }
         };
     }
 
-    private <T> ItscoBacker createBacker(final Configuration context, Class<T> itscoClass) {
-        return new DefaultItscoBacker(context, itscoClass);
-    }
-
-    protected <T> FieldResolutionStrategy determineStrategy(final Class<T> lookupType) {
-        for (FieldResolutionStrategy strategy : strategies) {
-            if (strategy.supports(lookupType)) {
-                return strategy;
-            }
-        }
-        throw new IllegalStateException("No strategy to support type " + lookupType.getName());
-    }
-
-    private class DefaultItscoBacker<I> implements ItscoBacker<I> {
-
-        // contains values that handle reloading on their own - these are often more expensive to create so we
-        // don't want to recreate them on every get call
-        private final Map<String, Object> cachedValues;
-        private final Configuration context;
-        private I containing;
-
-        public DefaultItscoBacker(Configuration context, Class<I> itscoClass) {
-            this.context = context;
-            cachedValues = new HashMap<String, Object>();
-//            this.
-        }
-
-        @Override
-        public void setContaining(I containing) {
-            this.containing = containing;
-        }
-
-        @Override
-        public <T> T lookup(final String name, final Class<T> lookupType) {
-            this.validateState();
-            FieldResolutionStrategy resolutionStrategy = determineStrategy(lookupType);
-            if (resolutionStrategy.isContextBacked()) {
-                if (cachedValues.containsKey(name)) {
-                    return lookupType.cast(cachedValues.get(name));
-                }
-            }
-            final T resolved = resolutionStrategy.resolve(name, lookupType, context, containing);
-            if (resolved == null) {
-                throw new IllegalStateException("No value for " + name);
-            }
-            if (resolutionStrategy.isContextBacked()) {
-                cachedValues.put(name, resolved);
-            }
-            return resolved;
-        }
-
-        @Override
-        public <T> T lookup(final String name, final Class<T> lookupType, final T defaultValue) {
-            this.validateState();
-            FieldResolutionStrategy resolutionStrategy = determineStrategy(lookupType);
-            final T resolved = resolutionStrategy.resolve(name, lookupType, context, containing);
-            return resolved != null ? resolved : defaultValue;
-        }
-
-        private void validateState() {
-            if(this.containing == null) {
-                throw new IllegalStateException("A containing object must be set before the backer is usable.");
-            }
-        }
-
-    }
 }
